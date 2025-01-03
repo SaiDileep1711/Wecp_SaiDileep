9c57b7e092b1daf5640b441f56ca6f550eb18341
username <wecp-dummy@gmail.com>
2025-01-03 16:36:03 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/entity/Team.java
+++ backend/src/main/java/com/wecp/progressive/entity/Team.java
@@ -1,15 +1,20 @@
 package com.wecp.progressive.entity;
-
-public class Team {
+ 
+import javax.persistence.*;
+ 
+@Entity
+public class Team implements Comparable<Team>{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int teamId;
     private String teamName;
     private String location;
     private String ownerName;
     private int establishmentYear;
-
+   
     public Team() {
     }
-
+   
     public Team(int teamId, String teamName, String location, String ownerName, int establishmentYear) {
         this.teamId = teamId;
         this.teamName = teamName;
@@ -17,48 +22,62 @@ public class Team {
         this.ownerName = ownerName;
         this.establishmentYear = establishmentYear;
     }
-
+   
+ 
+ 
     public int getTeamId() {
         return teamId;
     }
-
+ 
+ 
     public void setTeamId(int teamId) {
         this.teamId = teamId;
     }
-
+ 
+ 
     public String getTeamName() {
         return teamName;
     }
-
+ 
+ 
     public void setTeamName(String teamName) {
         this.teamName = teamName;
     }
-
+ 
+ 
     public String getLocation() {
         return location;
     }
-
+ 
+ 
     public void setLocation(String location) {
         this.location = location;
     }
-
+ 
+ 
     public String getOwnerName() {
         return ownerName;
     }
-
+ 
+ 
     public void setOwnerName(String ownerName) {
         this.ownerName = ownerName;
     }
-
+ 
+ 
     public int getEstablishmentYear() {
         return establishmentYear;
     }
-
+ 
+ 
     public void setEstablishmentYear(int establishmentYear) {
         this.establishmentYear = establishmentYear;
     }
-    
-
-    
-
+ 
+ 
+    @Override
+    public int compareTo(Team o) {
+            //  throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
+    return this.getTeamName().compareTo(o.getTeamName());
+        }
 }
